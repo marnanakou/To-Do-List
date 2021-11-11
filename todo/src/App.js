4b1ba305396c10dce4fc2b258e71ae8b4f2c3249
Maria Nanakou <marnanakou@gmail.com>
2021-11-11 20:03:07 +0100

    correcting the files syntax

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,11 @@
-// import Task from "./Components/Task";
+import Task from "./Components/Task/Task";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Task />
+    </div>
+  );
 }
 
 export default App;
