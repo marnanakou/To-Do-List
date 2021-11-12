a06f6bb97f7dee8930041ea4f106477b39168b89
Maria Nanakou <marnanakou@gmail.com>
2021-11-12 22:05:27 +0100

    create newTask

--- todo/src/Components/NewTask/NewTask.js
+++ todo/src/Components/NewTask/NewTask.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-import Task from "../Task";
+import addNewTask from "../Task";
 
 function CreateNewTask({ addNewTask }) {
-  return <div></div>;
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addNewTask();
+    // setValue(addNewTask);
+  };
+  return (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default CreateNewTask;
