8be797220103697dfa6de62df9999278cb321d9a
Maria Nanakou <marnanakou@gmail.com>
2021-11-12 15:19:48 +0100

    creating newTask and add state

--- todo/src/Components/Task/Task.js
+++ todo/src/Components/Task/Task.js
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import "./style.css";
 
 function Task() {
-  const [value, setValue] = useState();
-
-  const [tasks, seTasks] = useState([
+  const [tasks, setTasks] = useState([
     {
       title: "Go to the Gym",
       completed: false,
@@ -22,22 +20,40 @@ function Task() {
       completed: false,
     },
     {
-      title: "Language ready for my trip to Vienna",
+      title: "Handback ready for my trip to Vienna",
       completed: false,
     },
   ]);
+
+  const addNewTask = () => {
+    setTasks([...tasks]);
+  };
+
+  const [showNewTask, setShowNewTask] = useState(false);
+
+  //the second container appears after the click on the 1st one
+  const ShowNewTaskAdded = () => {
+    setShowNewTask(true);
+  };
 
   return (
     <div>
       <div className="header">List-To-Do</div>
       <div className="container">
-        <input type="text" placeholder="What needs to be accompliced?" />
+        <Task
+          type="text"
+          placeholder="What needs to be accompliced?"
+          value={addNewTask}
+        />
       </div>
       <ul>
         {tasks.map((tasks, index) => (
           <li key={index} tasks={tasks}></li>
         ))}
       </ul>
+      <div>
+        <button onClick={ShowNewTaskAdded}></button>
+      </div>
     </div>
   );
 }
