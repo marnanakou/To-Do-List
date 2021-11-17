8a4a45a90c2483065779a3ac3e7c4c209133b0bf
Maria Nanakou <marnanakou@gmail.com>
2021-11-17 18:31:29 +0100

    clear the files

--- todo/src/Components/Task/Task.js
+++ todo/src/Components/Task/Task.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import "./style.css";
 
+function TaskToDo(task) {
+  return (
+    <div
+      className="task"
+      style={{ textDecoration: task.completed ? "line-through" : " " }}
+    >
+      {task.title}
+    </div>
+  );
+}
+
 function Task() {
   const [tasks, setTasks] = useState([
     {
@@ -25,34 +36,31 @@ function Task() {
     },
   ]);
 
-  const addNewTask = (title) => {
-    setTasks([...tasks, { title: title, completed: false }]);
-  };
+  // const addNewTask = (title) => {
+  //   setTasks([...tasks, { title: title, completed: false }]);
+  // };
 
-  const [showNewTask, setShowNewTask] = useState(false);
+  // const [showNewTask, setShowNewTask] = useState(false);
 
-  //the second container appears after the click on the 1st one
-  const showNewTaskAdded = () => {
-    setShowNewTask(true);
-  };
+  // //the second container appears after the click on the 1st one
+  // const showNewTaskAdded = () => {
+  //   addNewTask(value);
+  //   setShowNewTask(" ");
+  // };
 
   return (
     <div>
       <div className="header">List-To-Do</div>
       <div className="container">
-        <input
-          type="text"
-          placeholder="What needs to be accompliced?"
-          value={addNewTask}
-        />
+        <input type="text" placeholder="What needs to be accompliced?" />
         <div className="tasks">
-          {tasks.map((el, index) => (
-            <li key={index}>{el.title}</li>
+          {tasks.map((task, index) => (
+            <Task key={index} index={index} task={task}></Task>
           ))}
         </div>
-        <div>
+        {/* <div>
           <button onClick={showNewTaskAdded}></button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
